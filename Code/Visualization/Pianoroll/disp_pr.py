1fa88574645f6352e6bc4b8aeaf57cda9059f29b
Leo Crestel <crestel.leopold@gmail.com>
2015-12-15 18:21:40 +0100

    Script o automatically open an html piano roll

--- Code/Visualization/Pianoroll/disp_pr.py
+++ Code/Visualization/Pianoroll/disp_pr.py
@@ -3,8 +3,9 @@
 # Script which open the proper html/D3.js file given a name and instrument
 import sys
 import shutil
-import os
+from subprocess import call
 
 path_to_csv = sys.argv[1]  # Whole path to the CSV file containing the data
 local_file = 'data.csv'
 shutil.copy(path_to_csv, local_file)
+call(["open", "pianoroll.html"])

887b3fdcf6abdacb10a8ab06cd3b50840d6a1c3d
Leo Crestel <crestel.leopold@gmail.com>
2016-01-08 13:53:37 +0100

    Start writing an predictive orchestral version of the RBM

--- Code/Models/RBM/rbm_orch_pred.py
+++ Code/Models/RBM/rbm_orch_pred.py
@@ -150,12 +150,11 @@ class RBM_orch(object):
     def propdown(self, hid):
         pre_sigmoid_activation = T.dot(hid, self.W.T) + self.vbias
         pre_sigmoid_activation_past = T.dot(hid, self.W_past.T) + self.vbias_past
-        return [pre_sigmoid_activation, pre_sigmoid_activation_past, T.nnet.sigmoid(pre_sigmoid_activation), T.nnet.sigmoid(pre_sigmoid_activation_past)]
+        return [pre_sigmoid_activation, T.nnet.sigmoid(pre_sigmoid_activation), pre_sigmoid_activation_past, T.nnet.sigmoid(pre_sigmoid_activation_past)]
 
     def sample_v_given_h(self, h0_sample):
-        ''' This function infers state of visible units given hidden units '''
         # compute the activation of the visible given the hidden sample
-        pre_sigmoid_v1, v1_mean = self.propdown(h0_sample)
+        pre_sigmoid_v1, v1_mean, pre_sigmoid_v1_past, v1_mean_past = self.propdown(h0_sample)
         # get a sample of the visible given their activation
         # Note that theano_rng.binomial returns a symbolic sample of dtype
         # int64 by default. If we want to keep our computations in floatX
@@ -163,4 +162,107 @@ class RBM_orch(object):
         v1_sample = self.theano_rng.binomial(size=v1_mean.shape,
                                              n=1, p=v1_mean,
                                              dtype=theano.config.floatX)
-        return [pre_sigmoid_v1, v1_mean, v1_sample]
+        v1_sample_past = self.theano_rng.binomial(size=v1_mean_past.shape,
+                                                  n=1, p=v1_mean_past,
+                                                  dtype=theano.config.floatX)
+        return [pre_sigmoid_v1, v1_mean, v1_sample, pre_sigmoid_v1_past, v1_mean_past, v1_sample_past]
+
+    def gibbs_hvh(self, h0_sample):
+        pre_sigmoid_v1, v1_mean, v1_sample, pre_sigmoid_v1_past, v1_mean_past, v1_sample_past = self.sample_v_given_h(h0_sample)
+        pre_sigmoid_h1, h1_mean, h1_sample = self.sample_h_given_v(v1_sample, v1_sample_past)
+        return [pre_sigmoid_v1, v1_mean, v1_sample,
+                pre_sigmoid_v1_past, v1_mean_past, v1_sample_past,
+                pre_sigmoid_h1, h1_mean, h1_sample]
+
+    def gibbs_vhv(self, v0_sample, v0_sample_past):
+        pre_sigmoid_h1, h1_mean, h1_sample = self.sample_h_given_v(v0_sample, v0_sample_past)
+        pre_sigmoid_v1, v1_mean, v1_sample, pre_sigmoid_v1_past, v1_mean_past, v1_sample_past = self.sample_v_given_h(h1_sample)
+        return [pre_sigmoid_h1, h1_mean, h1_sample,
+                pre_sigmoid_v1, v1_mean, v1_sample,
+                pre_sigmoid_v1_past, v1_mean_past, v1_sample_past]
+
+    def get_cost_updates(self, lr=0.1, persistent=None, k=1):
+        # positive phase
+        pre_sigmoid_ph, ph_mean, ph_sample = self.sample_h_given_v(self.input, self.past)
+        if persistent is None:
+            chain_start = ph_sample
+        else:
+            chain_start = persistent
+
+        # negative phase
+        (
+            [
+                pre_sigmoid_nvs,
+                nv_means,
+                nv_samples,
+                pre_sigmoid_nvs_past,
+                nv_means_past,
+                nv_samples_past,
+                pre_sigmoid_nhs,
+                nh_means,
+                nh_samples
+            ],
+            updates
+        ) = theano.scan(
+            self.gibbs_hvh,
+            # the None are place holders, saying that
+            # chain_start is the initial state corresponding to the
+            # 6th output
+            outputs_info=[None, None, None, None, None, None, None, None, chain_start],
+            n_steps=k
+        )
+        chain_end = nv_samples[-1]
+        chain_end_past = nv_samples_past[-1]
+
+        # Cost
+        cost = T.mean(self.free_energy(self.input, self.past)) - T.mean(
+            self.free_energy(chain_end, chain_end_past))
+        # Gradient
+        gparams = T.grad(cost, self.params, consider_constant=[chain_end, chain_end_past])
+        # Updates
+        for gparam, param in zip(gparams, self.params):
+            # make sure that the learning rate is of the right dtype
+            updates[param] = param - gparam * T.cast(
+                lr,
+                dtype=theano.config.floatX
+            )
+        if persistent:
+            # Note that this works only if persistent is a shared variable
+            updates[persistent] = nh_samples[-1]
+            # pseudo-likelihood is a better proxy for PCD
+            monitoring_cost = self.get_pseudo_likelihood_cost(updates)
+        else:
+            # reconstruction cross-entropy is a better proxy for CD
+            monitoring_cost = self.get_reconstruction_cost(updates,
+                                                           pre_sigmoid_nvs[-1])
+
+        return monitoring_cost, updates
+
+    def inpaint(self, v0_sample):
+        # Generate input unit given the past
+        (
+            [
+                pre_sigmoid_nvs,
+                nv_means,
+                nv_samples,
+                pre_sigmoid_nhs,
+                nh_means,
+                nh_samples
+            ],
+            updates
+        ) = theano.scan(
+            self.gibbs_vhv_inpaint,
+            # the None are place holders, saying that
+            # chain_start is the initial state corresponding to the
+            # 6th output
+            outputs_info=[None, None, None, None, None, chain_start],
+            non_sequences=[self.nv_samples_past],
+            n_steps=k
+        )
+
+    def gibbs_vhv_inpaint(self, v0_sample):
+
+    def prediction_error(self):
+        # Mean square error between predicted sample and original
+        input_predicted = inpaint()
+        (self.input - input_predicted) ** 2).sum()

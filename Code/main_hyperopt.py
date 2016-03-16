2e486767adcfd5b36086afe3022d6d4a9e0bf22e
Leo Crestel Mac <crestel.leopold@gmail.com>
2016-03-16 01:21:26 +0100

    Motif main (result folder)

--- Code/main_hyperopt.py
+++ Code/main_hyperopt.py
@@ -4,34 +4,32 @@
 # Main script for LOP
 import os
 
-import theano
-theano.config.compute_test_value = 'warn'
-
 # Select a model (path to the .py file)
 # Two things define a model : it's architecture and the time granularity
-from Models.Temporal_RBM.train_hopt import train_hopt
-model_name = u'Temporal_RBM'
+from Models.CRBM.train_hopt import train_hopt
+model_name = u'CRBM'
 temporal_granularity = u'frame_level'
 
-# Log file
+# Get main dir
 MAIN_DIR = os.getcwd().decode('utf8') + u'/'
-log_file_path = MAIN_DIR + u'log'
 
 # Build the matrix database (stored in a data.p file in Data) from a music XML database
 dataset = MAIN_DIR + u'../Data/data.p'
 
 # Set hyperparameters (can be a grid)
-result_file = u'../Results/' + temporal_granularity +\
-              '/' + model_name + u'/hopt_results.csv'
+result_folder = MAIN_DIR + u'../Results/' + temporal_granularity + '/' + model_name
+result_file = result_folder + u'/hopt_results.csv'
+log_file_path = result_folder + u'log'
 
 # Config is set now, no need to modify source below for standard use
 ############################################################################
 ############################################################################
 
+max_evals = 50  # number of hyper-parameter configurations evaluated
 
-########################################################################
-# Train step
-########################################################################
-max_evals = 10  # number of hyper-parameter configurations evaluated
+# Check is the result folder exists
+if not os.path.isdir(result_folder):
+    os.mkdir(result_folder)
+
 best = train_hopt(temporal_granularity, dataset, max_evals, log_file_path, result_file)
 print best

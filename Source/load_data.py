3e78f1c6bbc0eb66eb0d4ad22a7655d1fc236298
Leo Crestel Mac <crestel.leopold@gmail.com>
2016-09-15 15:06:27 +0200

    Simplified version of the load_data

--- Source/load_data.py
+++ Source/load_data.py
@@ -2,135 +2,86 @@
 # -*- coding: utf8 -*-
 
 import numpy as np
-import cPickle
 import theano
-import math
 
-from acidano.data_processing.minibatch_builder import k_fold_cross_validation, tvt_minibatch, tvt_minibatch_seq
-from acidano.data_processing.event_level import get_event_ind
-
-
-def get_data(data_path, temporal_granularity, temporal_order, sequential_learning=False, shared_bool=True, bin_unit_bool=True):
-    """
-    Load data from pickle (.p) file into a matrix. Return valid indexes for sequential learning
-
-    data_path:      relative path to pickelized data
-    temporal_order: temporal order of the model for which data are loaded
-
-    data :          shared variable containing the pianoroll representation of all the file concatenated in
-                    a single huge pr
-    valide_indexes: list of valid training point given the temporal order
-
-    """
-
-    orchestra_path = data_path + 'pr_orchestra.csv'
-    pr_orchestra = cPickle.load(open(orchestra_path, "rb"))
-    piano_path = data_path + 'pr_piano.csv'
-    pr_piano = cPickle.load(open(piano_path, "rb"))
-    metadata_path = data_path + 'metadata.pkl'
-    metadata = cPickle.load(open(metadata_path, "rb"))
-    new_track_ind = metadata['change_track']
-
-    # Build valid indices for building minibatches
-    pad_indices = []
-    T = pr_orchestra.shape[0]
-    l1 = new_track_ind[:]  # [:] = copy by value
-    l2 = new_track_ind[:]
-    l2.append(T)
-    l2.pop(0)
-    # Two cases :
-    if sequential_learning:
-        # RNN-like models: chunk of size temporal_order, overlap 50%
-        for s, e in zip(l1, l2):
-            step = math.floor(temporal_order / 2)
-            for i in range(s + temporal_order, e, step):
-                pad_indices.append(i)
-    else:
-        # others
-        for s, e in zip(l1, l2):
-            pad_indices.extend(range(s + temporal_order, e))
-
-    # Temporal granularity
-    if temporal_granularity == 'frame_level':
-        valid_indices = pad_indices
-    elif temporal_granularity == 'event_level':
-        new_event_ind = get_event_ind(pr_orchestra)
-        # Valid indices are the intersection of new event and valid
-        # Note that this operation shuffle the indices
-        valid_indices = set(new_event_ind).intersection(pad_indices)
-    elif temporal_granularity == 'full_event_level':
-        new_event_ind = get_event_ind(pr_orchestra)
-        # Reduce pr
-        pr_orchestra = pr_orchestra[new_event_ind, :]
-        pr_piano = pr_piano[new_event_ind, :]
-        # Compute valid indices
-        l3 = new_track_ind[:]
-        l3.append(T)
-        valid_indices = full_event_level_ind(l3, new_event_ind, temporal_order)
-
-    # Cast valid_index in a numpy array
-    valid_indices = np.array(valid_indices)
-
-    # Binary representation
-    if bin_unit_bool:
-        pr_orchestra = (pr_orchestra > 0).astype(int)
-        pr_piano = (pr_piano > 0).astype(int)
-
-    if shared_bool:
-        # Instanciate shared variables
-        orch_shared = theano.shared(np.asarray(pr_orchestra, dtype=theano.config.floatX))
-        piano_shared = theano.shared(np.asarray(pr_piano, dtype=theano.config.floatX))
-    else:
-        orch_shared = pr_orchestra
-        piano_shared = pr_piano
-
-    return orch_shared, piano_shared, valid_indices
+import logging
+import random
+import cPickle as pickle
 
 
-def full_event_level_ind(new_track_ind, new_event_ind, temporal_order):
-    m = 0
-    n = 0
-    counter = 0
-    out_list = []
-    for elem in new_event_ind:
-        if new_track_ind[n] <= elem:
-            n += 1
-            counter = 1
-        else:
-            if counter >= temporal_order:
-                out_list.append(m)
-            counter += 1
-        m += 1
-    return out_list
+def load_data(temporal_order=20, batch_size=100, binary_unit=True, skip_sample=1,logger_load=None):
+    # If no logger, create one
+    if logger_load is None:
+        logging.basicConfig(level=logging.INFO,
+                            format='%(asctime)s %(name)-12s %(levelname)-8s %(message)s',
+                            datefmt='%m-%d %H:%M',
+                            filename='load.log',
+                            filemode='w')
+        logger_load=logging.getLogger('load')
 
+    piano_train = np.load('../Data/piano_train.csv')
+    orchestra_train = np.load('../Data/orchestra_train.csv')
+    piano_valid = np.load('../Data/piano_valid.csv')
+    orchestra_valid = np.load('../Data/orchestra_valid.csv')
+    piano_test = np.load('../Data/piano_test.csv')
+    orchestra_test = np.load('../Data/orchestra_test.csv')
 
-def load_data_k_fold(data_path, temporal_granularity, temporal_order, shared_bool, bin_unit_bool, minibatch_size, split=(0.7, 0.1, 0.2)):
-    orch, piano, valid_index = get_data(data_path, temporal_granularity, temporal_order, False, shared_bool, bin_unit_bool)
-    train_index, validate_index, test_index = k_fold_cross_validation(valid_index, minibatch_size, split)
-    # train_index, validate_index, test_index = tvt_minibatch(log_file_path, valid_index, minibatch_size, shuffle, split)
-    return orch, piano, train_index, validate_index, test_index
+    # Binary unit ?
+    if binary_unit:
+        piano_train[np.nonzero(piano_train)] = 1
+        orchestra_train[np.nonzero(orchestra_train)] = 1
+        piano_valid[np.nonzero(piano_valid)] = 1
+        orchestra_valid[np.nonzero(orchestra_valid)] = 1
+        piano_test[np.nonzero(piano_test)] = 1
+        orchestra_test[np.nonzero(orchestra_test)] = 1
 
+    # Shared variables : push data on GPU, memory problem for this dataset ??
+    # First check type
+    type_data = (piano_train.dtype != theano.config.floatX)
+    if type_data:
+        logger_load.warning('Incorrect data type for pianorolls')
+        logger_load.warning(str(piano_train.dtype))
+    # borrow=True avoid copying the entire matrix
+    piano_train_shared = theano.shared(piano_train, name='piano_train', borrow=True)
+    orchestra_train_shared = theano.shared(orchestra_train, name='orchestra_train', borrow=True)
+    piano_valid_shared = theano.shared(piano_valid, name='piano_valid', borrow=True)
+    orchestra_valid_shared = theano.shared(orchestra_valid, name='orchestra_valid', borrow=True)
+    piano_test_shared = theano.shared(piano_test, name='piano_test', borrow=True)
+    orchestra_test_shared = theano.shared(orchestra_test, name='orchestra_test', borrow=True)
 
-def load_data_tvt(data_path, temporal_granularity, temporal_order, shared_bool, bin_unit_bool, minibatch_size, split=(0.7, 0.1, 0.2)):
-    orch, piano, valid_index = get_data(data_path, temporal_granularity, temporal_order, False, shared_bool, bin_unit_bool)
-    # train_index, validate_index, test_index = k_fold_cross_validation(log_file_path, valid_index, minibatch_size, split)
-    train_index, validate_index, test_index = tvt_minibatch(valid_index, minibatch_size, 'block', split)
-    return orch, piano, train_index, validate_index, test_index
+    tracks_start_end_train = pickle.load(open('../Data/tracks_start_end_train.pkl', 'rb'))
+    tracks_start_end_valid = pickle.load(open('../Data/tracks_start_end_valid.pkl', 'rb'))
+    tracks_start_end_test = pickle.load(open('../Data/tracks_start_end_test.pkl', 'rb'))
 
+    # Get valid indices given start_track and temporal_order
+    def valid_indices(tracks_start_end):
+        valid_ind = []
+        for (start_track, end_track) in tracks_start_end.values():
+            valid_ind.extend(range(start_track+temporal_order-1, end_track, skip_sample))
+        return valid_ind
 
-def load_data_seq_tvt(data_path, temporal_granularity, temporal_order, shared_bool, bin_unit_bool, split=(0.7, 0.1, 0.2)):
-    # In this case minibatch_size is equal to temporal_granularity
-    orch, piano, valid_index = get_data(data_path, temporal_granularity, temporal_order, True, shared_bool, bin_unit_bool)
-    train_index, validate_index, test_index = tvt_minibatch_seq(valid_index, temporal_order, True, split)
-    return orch, piano, train_index, validate_index, test_index
+    def build_batches(valid_ind):
+        batches = []
+        position = 0
+        n_batch = int(len(valid_ind) // batch_size)
 
+        # Shuffle indices
+        random.shuffle(valid_ind)
 
-if __name__ == '__main__':
-    orch, orch_mapping, piano, piano_mapping, \
-        train_batch_ind, validate_batch_ind, test_batch_ind =\
-        load_data_tvt(data_path='../Data/data.p',
-                      temporal_granularity='full_event_level',
-                      temporal_order=8,
-                      shared_bool=True,
-                      bin_unit_bool=True,
-                      minibatch_size=100)
+        for i in range(n_batch):
+            batches.append(valid_ind[position:position+batch_size])
+            position += batch_size
+        return batches
+
+    train_index = valid_indices(tracks_start_end_train)
+    train_batches = build_batches(train_index)
+
+    valid_index = valid_indices(tracks_start_end_valid)
+    valid_batches = build_batches(valid_index)
+
+    test_index = valid_indices(tracks_start_end_test)
+    test_batches = build_batches(test_index)
+
+    return piano_train_shared, orchestra_train_shared, np.asarray(train_batches, dtype=np.int32),\
+        piano_valid_shared, orchestra_valid_shared, np.asarray(valid_batches, dtype=np.int32),\
+        piano_test_shared, orchestra_test_shared, np.asarray(test_batches, dtype=np.int32)

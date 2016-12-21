4bc9d5779de8dce99b05ab102a636ad2ca20d798
Leo Crestel IRCAM <leopold.crestel@ircam.fr>
2016-12-21 13:13:33 +0100

    Generate midi files after training

---
+++ Source/generate.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+# -*- coding: utf8 -*-
+
+import cPickle as pkl
+from reconstruct_pr import reconstruct_pr
+from acidano.data_processing.midi.write_midi import write_midi
+
+
+def generate(model,
+             piano, orchestra, indices, metadata_path,
+             generation_length, seed_size, quantization_write,
+             generated_folder, logger_generate):
+    # Generate sequences from a trained model
+    # piano, orchestra and index are data used to seed the generation
+    # Note that generation length is fixed by the length of the piano input
+    logger_generate.info("# Generating")
+
+    generate_sequence = model.get_generate_function(
+        piano=piano, orchestra=orchestra,
+        generation_length=generation_length,
+        seed_size=seed_size,
+        batch_generation_size=len(indices),
+        name="generate_sequence")
+
+    # Load the mapping between pitch space and instrument
+    metadata = pkl.load(open(metadata_path, 'rb'))
+    instru_mapping = metadata['instru_mapping']
+
+    # Given last indices, generate a batch of sequences
+    (generated_sequence,) = generate_sequence(indices)
+    if generated_folder is not None:
+        for write_counter in xrange(generated_sequence.shape[0]):
+            # Write midi
+            pr_orchestra = reconstruct_pr(generated_sequence[write_counter], instru_mapping)
+            write_path = generated_folder + '/' + str(write_counter) + '.mid'
+            write_midi(pr_orchestra, quantization_write, write_path, tempo=80)
+
+    return
---
+++ Source/generate_results_folder.py
@@ -0,0 +1,93 @@
+#!/usr/bin/env python
+# -*- coding: utf8 -*-
+
+import glob
+import os
+import cPickle as pkl
+import time
+# Load data
+from load_data import load_data_test
+from generate import generate
+import logging
+
+def generate_midi(path, config=None, generation_length=50, seed_size=10, quantization_write=None):
+    # Generate midi files for a given path, which correspond to a set of training parameters :
+    # model, optimisation, granularity, conitous/discrete, quantization
+    # If config is None, process all configurations
+
+    # log file
+    ############################################################
+    # Logging
+    ############################################################
+    log_file_path = 'generate_log'
+    # set up logging to file - see previous section for more details
+    logging.basicConfig(level=logging.INFO,
+                        format='%(asctime)s %(name)-12s %(levelname)-8s %(message)s',
+                        datefmt='%m-%d %H:%M',
+                        filename=log_file_path,
+                        filemode='w')
+    # define a Handler which writes INFO messages or higher to the sys.stderr
+    console = logging.StreamHandler()
+    console.setLevel(logging.INFO)
+    # set a format which is simpler for console use
+    formatter = logging.Formatter('%(name)-12s: %(levelname)-8s %(message)s')
+    # tell the handler to use this format
+    console.setFormatter(formatter)
+    # add the handler to the root logger
+    logging.getLogger('').addHandler(console)
+    logger_generate = logging.getLogger('generate')
+
+    ############################################################
+    # Grab config files
+    ############################################################
+    if config:
+        config_list = [config]
+    else:
+        config_list = glob.glob(path + '/*')
+
+    for config_folder in config_list:
+        ############################################################
+        # Load the model and config
+        ############################################################
+        model_path = config_folder + '/model.pkl'
+        model = pkl.load(open(model_path, 'rb'))
+        param_path = config_folder + 'config.pkl'
+        space = pkl.load(open(param_path, 'rb'))
+        model_param = space['model']
+        script_param = space['script']
+        metadata_path = script_param['data'] + '/metadata.pkl'
+
+        if quantization_write is None:
+            quantization_write = script_param['quantization']
+
+        ############################################################
+        # Create generation folder
+        ############################################################
+        generated_folder = config_folder + '/generated_sequences'
+        if not os.path.isdir(generated_folder):
+            os.makedirs(generated_folder)
+
+        ############################################################
+        # Load data
+        ############################################################
+        time_load_0 = time.time()
+        piano_test, orchestra_test, _, generation_index \
+            = load_data_test(script_param['data_folder'],
+                             model_param['temporal_order'],
+                             model_param['batch_size'],
+                             binary_unit=script_param['binary_unit'],
+                             skip_sample=script_param['skip_sample'],
+                             logger_load=logger_generate)
+        time_load_1 = time.time()
+        logger_generate.info('TTT : Loading data took {} seconds'.format(time_load_1-time_load_0))
+
+        ############################################################
+        # Generate
+        ############################################################
+        time_generate_0 = time.time()
+        generate(model,
+                 piano_test, orchestra_test, generation_index, metadata_path,
+                 generation_length, seed_size, quantization_write,
+                 generated_folder, logger_generate)
+        time_generate_1 = time.time()
+        logger_generate.info('TTT : Generating data took {} seconds'.format(time_generate_1-time_generate_0))

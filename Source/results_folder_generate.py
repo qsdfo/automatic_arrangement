9b9485158d8cf69922a7ffc97a10e03ec28466a6
Leo Crestel IRCAM <leopold.crestel@ircam.fr>
2017-01-20 18:08:05 +0100

    Added the full track generation, to test how the model perform on famous known examples

--- Source/results_folder_generate.py
+++ Source/results_folder_generate.py
@@ -7,8 +7,19 @@ import time
 # Load data
 from load_data import load_data_test
 from generate import generate
+import build_data_aux
+import numpy as np
 
-def generate_midi(config_folder, generation_length, seed_size, quantization_write, logger_generate):
+import theano
+import re
+
+from acidano.utils.init import shared_zeros
+
+def generate_midi(config_folder, data_folder, generation_length, seed_size, quantization_write, corruption_flag, logger_generate):
+    if logger_generate is None:
+        import logging
+        logging.basicConfig(level=logging.WARNING)
+        logger_generate = logging.getLogger('generate')
     ############################################################
     # Load the model and config
     ############################################################
@@ -18,24 +29,20 @@ def generate_midi(config_folder, generation_length, seed_size, quantization_writ
     space = pkl.load(open(param_path, 'rb'))
     model_param = space['model']
     script_param = space['script']
-    metadata_path = script_param['data_folder'] + '/metadata.pkl'
+    metadata_path = data_folder + '/metadata.pkl'
 
     if quantization_write is None:
         quantization_write = script_param['quantization']
 
     ############################################################
-    # Create generation folder
-    ############################################################
-    generated_folder = config_folder + '/generated_sequences'
-    if not os.path.isdir(generated_folder):
-        os.makedirs(generated_folder)
-
-    ############################################################
     # Load data
     ############################################################
     time_load_0 = time.time()
+    piano_checksum = model.checksum_database['piano_test']
+    orchestra_checksum = model.checksum_database['orchestra_test']
     piano_test, orchestra_test, _, generation_index \
-        = load_data_test(script_param['data_folder'],
+        = load_data_test(data_folder,
+                         piano_checksum, orchestra_checksum,
                          model_param['temporal_order'],
                          model_param['batch_size'],
                          binary_unit=script_param['binary_unit'],
@@ -45,11 +52,111 @@ def generate_midi(config_folder, generation_length, seed_size, quantization_writ
     logger_generate.info('TTT : Loading data took {} seconds'.format(time_load_1-time_load_0))
 
     ############################################################
+    # Corruption and create generation folder
+    ############################################################
+    generated_folder = config_folder + '/generated_sequences'
+    if 'piano' == corruption_flag:
+        piano_test = shared_zeros(piano_test.get_value(borrow=True).shape)
+        generated_folder = generated_folder + '_corrupted_piano'
+        logger_generate.info("CORRUPTED PIANO :\nGenerating data with piano set to O")
+    elif 'orchestra' == corruption_flag:
+        orchestra_test = shared_zeros(orchestra_test.get_value(borrow=True).shape)
+        generated_folder = generated_folder + '_corrupted_orchestra'
+        logger_generate.info("CORRUPTED ORCHESTRA :\nGenerating data with orchestra seed set to O")
+    else:
+        logger_generate.info("NO CORRUPTION OF THE DATA :\nnormal generation")
+    if not os.path.isdir(generated_folder):
+        os.makedirs(generated_folder)
+
+    ############################################################
+    # Generate
+    ############################################################
+    time_generate_0 = time.time()
+
+    generate(model,
+             piano_test, orchestra_test, generation_index, metadata_path,
+             generation_length, seed_size, quantization_write,
+             generated_folder, logger_generate)
+    time_generate_1 = time.time()
+    logger_generate.info('TTT : Generating data took {} seconds'.format(time_generate_1-time_generate_0))
+
+def generate_midi_full_track_reference(config_folder, data_folder, track_path, seed_size, quantization_write, number_of_version, logger_generate):
+    # This function generate the orchestration of a full track
+    if logger_generate is None:
+        import logging
+        logging.basicConfig(level=logging.WARNING)
+        logger_generate = logging.getLogger('generate')
+    ############################################################
+    # Load the model and config
+    ############################################################
+    model_path = config_folder + '/model.pkl'
+    model = pkl.load(open(model_path, 'rb'))
+    param_path = config_folder + '/config.pkl'
+    space = pkl.load(open(param_path, 'rb'))
+    script_param = space['script']
+    metadata_path = data_folder + '/metadata.pkl'
+
+    if quantization_write is None:
+        quantization_write = script_param['quantization']
+
+    ############################################################
+    # Read piano midi file
+    ############################################################
+    # Get dictionnary representing the aligned midi tracks
+    piano_test_dict, instru_piano, name_piano, orchestra_test_dict, instru_orchestra, name_orchestra, duration\
+        = build_data_aux.process_folder(track_path, quantization_write, script_param['temporal_granularity'], logger_generate)
+
+    # Get the instrument mapping used for training
+    metadata = pkl.load(open(metadata_path, 'rb'))
+    instru_mapping = metadata['instru_mapping']
+
+    # Instanciate track pianoroll
+    N_orchestra = metadata['N_orchestra']
+    N_piano = instru_mapping['piano']['index_max']
+    pr_orchestra = np.zeros((duration, N_orchestra), dtype=np.float32)
+    pr_piano = np.zeros((duration, N_piano), dtype=np.float32)
+
+    # Fill them with the dictionnary representation of the track, respecting the instrument mapping
+    pr_piano = build_data_aux.cast_small_pr_into_big_pr(piano_test_dict, {}, 0, duration, instru_mapping, pr_piano)
+    pr_orchestra = build_data_aux.cast_small_pr_into_big_pr(orchestra_test_dict, instru_orchestra, 0, duration, instru_mapping, pr_orchestra)
+
+    # Is it binary units ?
+    binary_unit = script_param['binary_unit']
+    if binary_unit:
+        pr_piano[np.nonzero(pr_piano)] = 1
+        pr_orchestra[np.nonzero(pr_orchestra)] = 1
+    else:
+        # Much easier to work with unit between 0 and 1, for several reason :
+        #       - 'normalized' values
+        #       - same reconstruction as for binary units when building midi files
+        pr_piano = pr_piano / 127
+        pr_orchestra = pr_orchestra / 127
+
+    # Push them on the GPU
+    pr_piano_shared = theano.shared(pr_piano, name='piano_generation', borrow=True)
+    pr_orchestra_shared = theano.shared(pr_orchestra, name='orchestra_generation', borrow=True)
+
+    # Generation parameters
+    generation_length = duration - 150
+    seed_size = 20
+    # generation_index is the last index of the track we want to generate
+    # We feed several time the same index to get different proposition of orchestration
+    generation_index = np.asarray([duration-1,] * number_of_version, dtype=np.int32)
+
+    # Store folder
+    string = re.split(r'/', name_piano)[-1]
+    name_track = re.sub('piano_solo.mid', '', string)
+
+    generated_folder = config_folder + '/generation_reference_example/' + name_track
+    if not os.path.isdir(generated_folder):
+        os.makedirs(generated_folder)
+
+    ############################################################
     # Generate
     ############################################################
     time_generate_0 = time.time()
     generate(model,
-             piano_test, orchestra_test, generation_index, metadata_path,
+             pr_piano_shared, pr_orchestra_shared, generation_index, metadata_path,
              generation_length, seed_size, quantization_write,
              generated_folder, logger_generate)
     time_generate_1 = time.time()
--- Source/results_folder_main.py
+++ Source/results_folder_main.py
@@ -1,14 +1,6 @@
 #!/usr/bin/env python
 # -*- coding: utf8 -*-
 
-
-import glob
-import logging
-
-from results_folder_generate import generate_midi
-from results_folder_plot_weights import plot_weights
-from results_folder_csv import get_results_and_id, write_csv_results
-
 # Main script for processing results folders
 # Processing consists in :
 #   per folder processing :
@@ -17,48 +9,94 @@ from results_folder_csv import get_results_and_id, write_csv_results
 #   "global" processing
 #       - write result.csv files, which sums up and sort the results of the different hyper-parameter configurations
 #       - plot hyper-parameter statistics
+#
+#
+#
+#
+# IMPORTANT :
+#   To avoid mixing training and generation files, and most importantly to be sure that the reconstruction is correctly done,
+#   USE EXACTLY THE SAME DATA FOLDER THAT THE ONE USED FOR training
+#   Hence, if trained on guillimin, scp it before doing any post processing
 
 
-def processing_results(path, generation_length=50, seed_size=20, quantization_write=None):
+import glob
+import logging
+
+from results_folder_generate import generate_midi, generate_midi_full_track_reference
+from results_folder_plot_weights import plot_weights
+from results_folder_csv import get_results_and_id, write_csv_results
+from corrupted import generate_corrupted_results
+
+############################################################
+# Logging
+############################################################
+log_file_path = 'generate_log'
+# set up logging to file - see previous section for more details
+logging.basicConfig(level=logging.INFO,
+                    format='%(asctime)s %(name)-12s %(levelname)-8s %(message)s',
+                    datefmt='%m-%d %H:%M',
+                    filename=log_file_path,
+                    filemode='w')
+# define a Handler which writes INFO messages or higher to the sys.stderr
+console = logging.StreamHandler()
+console.setLevel(logging.INFO)
+# set a format which is simpler for console use
+formatter = logging.Formatter('%(name)-12s: %(levelname)-8s %(message)s')
+# tell the handler to use this format
+console.setFormatter(formatter)
+# add the handler to the root logger
+logging.getLogger('').addHandler(console)
+logger_generate = logging.getLogger('generate')
+logger_plot = logging.getLogger('plot_weights')
+
+def processing_results(configurations_path, data_folder, track_paths, generation_length=200, seed_size=20, quantization_write=None):
     # path has to be the roots of the results folders
     # e.g. : /home/aciditeam-leo/Aciditeam/lop/Results/event_level/discrete_units/quantization_4/gradient_descent/LSTM
-
-    ############################################################
-    # Logging
-    ############################################################
-    log_file_path = 'generate_log'
-    # set up logging to file - see previous section for more details
-    logging.basicConfig(level=logging.INFO,
-                        format='%(asctime)s %(name)-12s %(levelname)-8s %(message)s',
-                        datefmt='%m-%d %H:%M',
-                        filename=log_file_path,
-                        filemode='w')
-    # define a Handler which writes INFO messages or higher to the sys.stderr
-    console = logging.StreamHandler()
-    console.setLevel(logging.INFO)
-    # set a format which is simpler for console use
-    formatter = logging.Formatter('%(name)-12s: %(levelname)-8s %(message)s')
-    # tell the handler to use this format
-    console.setFormatter(formatter)
-    # add the handler to the root logger
-    logging.getLogger('').addHandler(console)
-    logger_generate = logging.getLogger('generate')
-    logger_plot = logging.getLogger('plot_weights')
 
     ############################################################
     # Processing
     ############################################################
     # Get folders starting with a number
-    configurations = glob.glob(path + '/[0-9]*')
+    configurations = glob.glob(configurations_path + '/[0-9]*')
     id_result_list = []
+    number_of_version = 5
+
     for configuration in configurations:
-        generate_midi(configuration, generation_length, seed_size, quantization_write, logger_generate)
-        import pdb; pdb.set_trace()
-        plot_weights(configuration, logger_plot)
-        import pdb; pdb.set_trace()
+        # generate_midi(configuration, data_folder, generation_length, seed_size, quantization_write, None, logger_generate)
+        for track_path in track_paths:
+            generate_midi_full_track_reference(configuration, data_folder, track_path, seed_size, quantization_write, number_of_version, logger_generate)
+        # generate_corrupted_results(configuration, data_folder, generation_length, seed_size)
+        # plot_weights(configuration, logger_plot)
         id_result_list.append(get_results_and_id(configuration))
 
-    write_csv_results(id_result_list, configurations[0])
+    write_csv_results(configurations_path, id_result_list, configurations[0])
 
 if __name__ == '__main__':
-    processing_results('/home/aciditeam-leo/Aciditeam/lop/Results_bis/event_level/discrete_units/quantization_4/gradient_descent/RBM_inpainting')
+    # IMPORTANT :
+    #   To avoid mixing training and generation files, and most importantly to be sure that the reconstruction is correctly done,
+    #   USE EXACTLY THE SAME DATA FOLDER THAT THE ONE USED FOR training
+    #   Hence, if trained on guillimin, scp it before doing any post processing
+    data_folder = '/home/aciditeam-leo/Aciditeam/lop/Data'
+    model_path = '/home/aciditeam-leo/Aciditeam/lop/Results_gpu/event_level/discrete_units/quantization_4/gradient_descent/'
+    track_paths = [
+        '/home/aciditeam-leo/Aciditeam/database/Orchestration/Orchestration_checked/liszt_classical_archive/16',
+        '/home/aciditeam-leo/Aciditeam/database/Orchestration/Orchestration_checked/bouliane/22',
+    ]
+
+    # ccc = '/home/aciditeam-leo/Aciditeam/lop/Results_gpu/event_level/discrete_units/quantization_4/gradient_descent/cRnnRbm/3639643'
+    # eee = '/home/aciditeam-leo/Aciditeam/database/Orchestration/Orchestration_checked/bouliane/22'
+    # generate_midi_full_track_reference(ccc, data_folder, eee, 20, 4, 5, logger_generate)
+
+    for nn in ['cRBM', 'cRnnRbm', 'FGcRBM', 'LSTM', 'RBM_inpainting', 'RnnRbm_inpainting']:
+        model_path_this = model_path + '/' + nn
+        processing_results(model_path_this, data_folder, track_paths)
+
+    #################################
+    # Or just generate or plot weight of a specific configuration
+    #################################
+    # configuration = '/home/aciditeam-leo/Aciditeam/lop/Results_bis/event_level/discrete_units/quantization_4/gradient_descent/RBM_inpainting/6546512'
+    # generation_length = 50
+    # seed_size = 20
+    # quantization_write = 4
+    # logger_generate = logger_generate
+    # generate_midi(configuration, data_folder, generation_length, seed_size, quantization_write, logger_generate)

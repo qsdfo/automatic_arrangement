684b2d647c6438c28f3f0605f718787773d5912b
Leo Crestel IRCAM <leopold.crestel@ircam.fr>
2017-03-09 14:34:37 +0100

    Hparam plots w/ hover

--- Source/results_folder_hparam_plot.py
+++ Source/results_folder_hparam_plot.py
@@ -4,6 +4,13 @@
 import matplotlib.pyplot as plt
 import csv as csv
 import math
+import numpy as np
+
+from bokeh.io import output_file, save
+from bokeh.layouts import gridplot
+from bokeh.plotting import figure, ColumnDataSource
+from bokeh.models import HoverTool
+
 
 def read_csv_results(path):
     dico_hparam = {}
@@ -19,35 +26,42 @@ def read_csv_results(path):
 
 def plot_hparam(path):
     dico_hparam = read_csv_results(path)
-    accuracy_list = dico_hparam['accuracy']
+    accuracy_list = np.asarray([float(e) for e in dico_hparam['accuracy']])
+
+    # Color list
+    min_red = 38
+    min_green = 87
+    min_blue = 235
+    max_red = 222
+    max_green = 97
+    max_blue = 96
+    scaled_accuracy_list_blue = [(min_blue + (max_blue-min_blue)*e/max(accuracy_list)) for e in accuracy_list]
+    scaled_accuracy_list_green = [(min_green + (max_green-min_green)*e/max(accuracy_list)) for e in accuracy_list]
+    scaled_accuracy_list_red = [(min_red + (max_red-min_red)*e/max(accuracy_list)) for e in accuracy_list]
+    colors = [
+        "#%02x%02x%02x" % (int(r), int(g), int(b)) for r,g,b in zip(scaled_accuracy_list_blue, scaled_accuracy_list_green, scaled_accuracy_list_red)
+    ]
 
     avoid_hparam = ['accuracy', 'optim', 'ID', 'loss', 'model', 'lr']
-    hparam_name_to_plot = [e for e in dico_hparam.keys() if e not in avoid_hparam]
+    dico_hparam_to_plot = {e: dico_hparam[e] for e in dico_hparam.keys() if e not in avoid_hparam}
+    source = ColumnDataSource(
+            data=dico_hparam
+        )
 
-    grid_size = int(math.ceil(math.sqrt(len(hparam_name_to_plot))))
-    f, axarr = plt.subplots(grid_size, grid_size)
-    counterx = 0
-    countery = 0
+    list_plot = []
+    output_file(path + "/hparam.html")
 
-    for hparam_name in hparam_name_to_plot:
+    for hparam_name in dico_hparam_to_plot.keys():
+        hover = HoverTool(tooltips=[(e, "@" + e) for e in dico_hparam.keys()])
+        s = figure(title=hparam_name)
+        s.add_tools(hover)
+        s.xaxis.axis_label = hparam_name
+        s.yaxis.axis_label = "Accuracy"
+        s.circle(hparam_name, "accuracy", size=10, color=colors, source=source)
+        list_plot.append(s)
+    # make a grid
+    grid = gridplot(list_plot, ncols=2, plot_width=250, plot_height=250)
+    save(grid)
 
-        hparam_value = dico_hparam[hparam_name]
-
-        # Four axes, returned as a 2-d array
-        axarr[counterx, countery].plot(hparam_value, accuracy_list, 'bo')
-        axarr[counterx, countery].set_title(hparam_name, fontsize=14, fontweight='bold')
-        axarr[counterx, countery].set_xlabel('value of hparam')
-        axarr[counterx, countery].set_ylabel('Accuracy score')
-
-        counterx = counterx + 1
-        if counterx == grid_size:
-            counterx = 0
-            countery = countery + 1
-
-    f.subplots_adjust(hspace=0.58)
-    plt.savefig(path + '/hparam_name.pdf')
-    plt.close(f)
-
-
-if __name__ == '__main__':
-    plot_hparam("/home/aciditeam-leo/Aciditeam/lop/Results_guillimin/27_02_17/Results/event_level/discrete_units/quantization_100/gradient_descent/cRBM")
+if __name__ == "__main__":
+    plot_hparam("/home/aciditeam-leo/Aciditeam/lop/Results_guillimin/27_02_17/Results/event_level/binary/quantization_100/gradient_descent/cRBM/")

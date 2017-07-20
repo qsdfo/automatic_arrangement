bb3b8297b3acd0ae597ecab20e1be936b1c30d00
Leo Crestel IRCAM <leopold.crestel@ircam.fr>
2017-07-20 17:34:41 +0200

    Use a generator that yields batches

---
+++ Source/Database/generator.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+# -*- coding: utf-8-unix -*-
+
+
+def generate_in_background(generator, num_cached=50):
+    """
+    Runs any generator in a background thread, caching up to `num_cached` items.
+    """
+    import Queue
+    queue = Queue.Queue(maxsize=num_cached)
+    sentinel = object()  # guaranteed unique reference
+
+    # define producer (putting items into queue)
+    def producer():
+        for item in generator:
+            queue.put(item)
+        queue.put(sentinel)
+
+    # start producer (in a background thread)
+    import threading
+    thread = threading.Thread(target=producer)
+    thread.daemon = True
+    thread.start()
+
+    # run as consumer (read items from queue, in current thread)
+    item = queue.get()
+    while item is not sentinel:
+        yield item
+        queue.task_done()
+        item = queue.get()

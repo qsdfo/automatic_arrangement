4af28f43887826bb2f8c6cb9260d6cbb4699c5e1
Leo Crestel Mac <crestel.leopold@gmail.com>
2017-09-22 16:28:05 +0900

    Removed backup from repository

--- Source/LOP/Backup/assert_database.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python
-# -*- coding: utf8 -*-
-
-
-def assert_database(metadata, script_param):
-    assert metadata["temporal_granularity"] == script_param['temporal_granularity']
-    assert metadata["quantization"] == script_param['quantization']
-    assert metadata["max_translation"] == script_param['max_translation']
-    assert metadata["unit_type"] == script_param['unit_type']

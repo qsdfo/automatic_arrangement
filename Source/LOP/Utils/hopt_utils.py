55be00e720f0066803d5a536c3b163335c9cbbce
Leo Crestel Mac <crestel.leopold@gmail.com>
2017-11-04 12:40:23 +0900

    Hopts

--- Source/LOP/Utils/hopt_utils.py
+++ Source/LOP/Utils/hopt_utils.py
@@ -1,12 +1,21 @@
 #!/usr/bin/env python
 # -*- coding: utf8 -*-
 
-from LOP.Utils.hopt_wrapper import qloguniform_int
+from LOP.Utils.hopt_wrapper import qloguniform_int, quniform_int
 from hyperopt import hp
 from math import log
 
-def multi_layer_hopt(min_unit, max_unit, step, min_num_layer, max_num_layer, name):
+def list_log_hopt(min_unit, max_unit, step, min_num_layer, max_num_layer, name):
     return hp.choice(name, [
         [qloguniform_int(name+'_'+str(i), log(min_unit), log(max_unit), step) for i in range(num_layer)] \
             for num_layer in range(min_num_layer, max_num_layer)
             ])
+
+def list_hopt(min_unit, max_unit, step, min_num_layer, max_num_layer, name):
+	return hp.choice(name, [
+	    [quniform_int(name+'_'+str(i), min_unit, max_unit, step) for i in range(num_layer)] \
+	        for num_layer in range(min_num_layer, max_num_layer)
+	        ])
+
+def list_hopt_fixedSized(ranges, name):
+	return [quniform_int(name+'_'+str(i), min_unit, max_unit, step) for i, (min_unit, max_unit, step) in enumerate(ranges)]

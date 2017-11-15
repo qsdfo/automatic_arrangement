a35d3a1380fc64f223aa3f17a317285c90149601
Leo Crestel Mac <crestel.leopold@gmail.com>
2017-11-15 12:29:00 +0900

    Indentation fixed

--- Source/LOP/Models/Future_piano/model_lop_future_piano.py
+++ Source/LOP/Models/Future_piano/model_lop_future_piano.py
@@ -3,28 +3,13 @@
 
 from LOP.Models.model_lop import Model_lop
 
-# Tensorflow
-import tensorflow as tf
-
-# Keras
-import keras
-from keras.layers.recurrent import GRU
-from keras.layers import Dense
-
-# Hyperopt
-from LOP.Utils import hopt_wrapper
-from math import log
-from hyperopt import hp
-
-from LOP.Models.Utils.weight_summary import keras_layer_summary
-
 
 class MLFP(Model_lop):
-	
-	def __init__(self, model_param, dimensions):
-		Model_lop.__init__(self, model_param, dimensions)
-		return
+    
+    def __init__(self, model_param, dimensions):
+        Model_lop.__init__(self, model_param, dimensions)
+        return
 
-	@staticmethod
-	def name():
-		return "Future_piano_"
+    @staticmethod
+    def name():
+        return "Future_piano_"

02a507ad059444a428db4791d566c99e0a081a83
Leo Crestel Mac <crestel.leopold@gmail.com>
2017-11-22 19:12:36 +0900

    New analysis scripts. For experiments for comparing accuracy and Xentropy

---
+++ Source/LOP/Results_process/2D_measure_comparison.py
@@ -0,0 +1,69 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+"""
+Compare the surfaces of the different measures in a 2 dimensional case.
+Created on Wed Nov 22 15:30:57 2017
+
+@author: leo
+"""
+
+import os
+import shutil
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d.axes3d import Axes3D, get_test_data
+from matplotlib import cm
+import numpy as np
+from matplotlib.ticker import LinearLocator, FormatStrFormatter
+
+from LOP.Utils.measure import accuracy_measure, true_accuracy_measure, f_measure, binary_cross_entropy 
+
+def plot_measure_2D(measure_fun, step_size, result_folder):
+
+    fig = plt.figure()
+    
+    # Make data, avoiding 0 and 1 which raise problems
+    p1 = np.arange(step_size, 1-step_size, step_size) 
+    p2 = np.arange(step_size, 1-step_size, step_size)
+    N_dim = len(p1)
+    X, Y = np.meshgrid(p1, p2)
+    X_reshape = np.reshape(X, [-1,])
+    Y_reshape = np.reshape(Y, [-1,])
+    pred_frame_mat = np.stack((X_reshape,Y_reshape), axis=1)
+    
+    for ind, true_frame in enumerate([(0,0), (0,1), (1,0), (1,1)]):
+        
+        ax = fig.add_subplot(2, 2, ind+1, projection='3d')
+    
+        true_frame_ = np.reshape(true_frame, [-1, 1])
+        true_frame_mat = (true_frame_.repeat(N_dim * N_dim, 1)).T
+        measurement = measure_fun(true_frame_mat, pred_frame_mat)
+        Z = np.reshape(measurement, [N_dim, N_dim])
+        
+        # Plot the surface.
+        surf = ax.plot_surface(X, Y, Z, cmap=cm.coolwarm,
+                               linewidth=0, antialiased=False)
+        
+        # Customize the z axis.
+        ax.set_zlim(Z.min()-0.1, Z.max()+0.1)
+        ax.zaxis.set_major_locator(LinearLocator(10))
+        ax.zaxis.set_major_formatter(FormatStrFormatter('%.02f'))
+        
+        # Add a color bar which maps values to colors.
+        fig.colorbar(surf, shrink=0.5, aspect=5)
+        
+    plt.savefig(result_folder + '/' + measure_fun.__name__ + '__' + str(true_frame) + '.pdf')
+    plt.show()
+
+
+if __name__ == '__main__':
+    result_dir = "plot_measure_2D"
+    if os.path.isdir(result_dir):
+        shutil.rmtree(result_dir)
+    os.mkdir(result_dir)
+        
+#    plot_measure_2D(accuracy_measure, 0.01, result_dir)
+    plot_measure_2D(binary_cross_entropy, 0.01, result_dir)
+#    plot_measure_2D(true_accuracy_measure, (0,1), 0.01, result_dir)
+#    plot_measure_2D(f_measure, (0,1), 0.01, result_dir)
+#    plot_measure_2D(binary_cross_entropy, (0,1), 0.01, result_dir)
+    
---
+++ Source/LOP/Results_process/linear_regression_measures.py
@@ -0,0 +1,92 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Nov 22 14:52:16 2017
+Plot relation between two measures and their linear regression.
+
+@author: leo
+"""
+
+#!/usr/bin/env python
+# -*- coding: utf8 -*-
+
+
+from sklearn import datasets, linear_model
+from matplotlib import pyplot as plt
+from sklearn.metrics import mean_squared_error
+
+import os
+import shutil
+import numpy as np
+
+import LOP.Scripts.config as config
+
+def compare(x, name_measure_A, y, name_measure_B, result_folder):
+    """Compute the linear regression, scatter plot the points in a plan along with the curve derived from the linear reg.
+    
+    Parameters
+    ----------
+    x : 1D npy array
+        values corresponding to the first measure
+    y : 1D npy array
+        values corresponding to the second measure
+    
+    """
+    # Linear regression
+    regr = linear_model.LinearRegression()
+    regr.fit(np.reshape(x, (-1, 1)), y)
+    y_reg = regr.predict(np.reshape(x, (-1, 1)))
+    
+    # Create a trace
+    mse =  mean_squared_error(y, y_reg)
+    
+    plt.scatter(x, y, c="b", alpha=0.5)
+    plt.plot(x, y_reg, c="r")
+    plt.xlabel(name_measure_A)
+    plt.ylabel(name_measure_B)
+    plt.title("Linear regression between " + name_measure_A + " and " + name_measure_B + ".\n MSE = " + str(mse))
+    
+    # Save
+    plt.savefig(result_folder + '/' + name_measure_A + '_' + name_measure_B + '_compare.pdf')
+    return
+
+def process_measure(config_folders, name_measure_A, name_measure_B, result_folder):
+    """Gather the data from different configuration folders
+    
+    Paramaters
+    ----------
+    config_folders : str list
+        list of strings containing the configurations from which we want to collect results
+    name_measure_A : str
+        name of the first measure (has to match a .npy file in the corresponding configuration folder)
+    name_measure_B : str
+        name of the second measure
+        
+    """
+    measure_A_list = []
+    measure_B_list = []
+    for config_folder in config_folders:
+        # Read npy
+        measure_A_list.append(np.load(os.path.join(config_folder, name_measure_A)))
+        measure_B_list.append(np.load(os.path.join(config_folder, name_measure_B)))
+    measure_A = np.concatenate(measure_A_list)
+    measure_B = np.concatenate(measure_B_list)
+    compare(measure_A, name_measure_A, measure_B, name_measure_B)
+    return
+
+if __name__ == '__main__':
+    config_folders = [
+        '',
+    ]
+    result_folder = 'compare_measure'
+
+    if os.path.isdir(result_folder):
+        shutil.rmtree(result_folder)
+    os.mkdir(result_folder)
+        
+    process_measure(config_folders, "accuracy", "Xent", result_folder)
+    process_measure(config_folders, "true_accuracy", "Xent", result_folder)
+    process_measure(config_folders, "f_measure", "Xent", result_folder)
+    process_measure(config_folders, "true_accuracy", "accuracy", result_folder)
+    process_measure(config_folders, "f_measure", "accuracy", result_folder)
+    process_measure(config_folders, "f_measure", "true_accuracy", result_folder)

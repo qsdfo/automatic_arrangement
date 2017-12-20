0ad592a3016b0a9590cb05205f5376887277056c
Leo Crestel Mac <crestel.leopold@gmail.com>
2017-12-20 16:32:16 +0900

    Fixed early stopping

--- Source/LOP/Utils/early_stopping.py
+++ Source/LOP/Utils/early_stopping.py
@@ -15,7 +15,7 @@ def up_criterion(val_tab, epoch, number_strips=3, validation_order=2):
     while(UP and s < number_strips):
         t = epoch - s
         tmk = epoch - s - validation_order
-        UP = val_tab[t] > val_tab[tmk] * (1 - epsilon)  # Avoid extremely small evolutions
+        UP = val_tab[t] > val_tab[tmk] - epsilon * abs(val_tab[tmk])   # Avoid extremely small evolutions
         s = s + 1
         if not UP:
             OVERFITTING = False

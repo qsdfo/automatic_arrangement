e908fac53f334faa75f670c4f0bf184736eed818
Leo Crestel Mac <crestel.leopold@gmail.com>
2018-01-18 10:28:50 +0900

    Build valid indices for long range prediction

--- Source/LOP/Database/load_data_k_folds.py
+++ Source/LOP/Database/load_data_k_folds.py
@@ -3,7 +3,7 @@
 
 import random
 
-def build_folds(tracks_start_end, piano, orchestra, k_folds=10, temporal_order=20, batch_size=100, random_seed=None, logger_load=None):
+def build_folds(tracks_start_end, piano, orchestra, k_folds=10, temporal_order=20, batch_size=100, long_range_pred=1, random_seed=None, logger_load=None):
     list_files = tracks_start_end.keys()
 
     # Folds are built on files, not directly the indices
@@ -28,21 +28,25 @@ def build_folds(tracks_start_end, piano, orchestra, k_folds=10, temporal_order=2
             # Get valid indices for a track
             start_track, end_track = tracks_start_end[filename]
             ind = range(start_track+temporal_order-1, end_track-temporal_order+1)
+            ind_long_range = range(start_track+temporal_order-1, end_track-temporal_order-long_range_pred+1)
             counter_fold = counter + k
             if (counter_fold % k_folds) < k_folds-2:
                 train_ind.extend(ind)
             elif (counter_fold % k_folds) == k_folds-2:
                 this_valid_names.append(filename)
                 valid_ind.extend(ind)
+                valid_ind_long_range.extend(ind_long_range)
             elif (counter_fold % k_folds) == k_folds-1:
                 this_test_names.append(filename)
                 test_ind.extend(ind)
         train_ind_noSilence = remove_silences(train_ind, piano, orchestra)
         valid_ind_noSilence = remove_silences(valid_ind, piano, orchestra)
+        valid_ind_long_range_noSilence = remove_silences(valid_ind_long_range, piano, orchestra)
         test_ind_noSilence = remove_silences(test_ind, piano, orchestra)
         folds.append({'train': build_batches(train_ind_noSilence, batch_size), 
                       'test': build_batches(test_ind_noSilence, batch_size), 
-                      'valid': build_batches(valid_ind_noSilence, batch_size)})
+                      'valid': build_batches(valid_ind_noSilence, batch_size),
+                      'valid_long_range': build_batches(valid_ind_long_range_noSilence, batch_size)})
         valid_names.append(this_valid_names)
         test_names.append(this_test_names)
     return folds, valid_names, test_names

b1aa20d0d6f963acc148bc48ff8803fcb355b9d7
Leo Crestel Mac <crestel.leopold@gmail.com>
2018-02-01 11:30:59 +0900

    Update in core function for learning

--- Source/LOP/Models/Utils/mlp.py
+++ Source/LOP/Models/Utils/mlp.py
@@ -5,9 +5,9 @@ import tensorflow as tf
 from keras.layers import Dense
 from LOP.Models.Utils.weight_summary import keras_layer_summary
 
-def MLP(x, layers, scope_name, activation='relu'):
+def MLP(x, layers, activation='relu'):
 	for layer_ind, num_unit in enumerate(layers):
-		with tf.variable_scope(scope_name + "_" + str(layer_ind)):
+		with tf.variable_scope(str(layer_ind)):
 			dense_layer = Dense(num_unit, activation=activation)
 			x = dense_layer(x)
 			keras_layer_summary(dense_layer)
